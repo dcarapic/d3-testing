5214b14704a3d4219e9ff52d3d82c73eae106ef4
Dalibor Čarapić <dcarapic@gmail.com>
2022-06-11 15:27:38 +0200

    Playing around

--- d3js-vue-chart/src/components/Chart.vue
+++ d3js-vue-chart/src/components/Chart.vue
@@ -1,38 +1,83 @@
 <template>
     <div class="container">
-        <div v-if="!isFinished">Loading stock data</div>
-        <div v-if="error">An error occurred while loading stock data: {{ error }}</div>
-        <div v-if="isFinished" class="setting">
-            <label for="stocks">Stocks count</label><input type="number" v-model="stockNumber"><span>(Total: {{
-                    allRows.length
-            }})</span>
+        <div v-if="!fetchStocks.isFinished.value">Loading stock data</div>
+        <div v-if="fetchStocks.error.value">An error occurred while loading stock data: {{ fetchStocks.error.value }}
         </div>
-        <div id="chart"></div>
+        <div v-if="fetchStocks.isFinished.value" class="setting">
+            <label for="stocks">Stocks count</label><input type="number" v-model.lazy="stockNumber" min="10"
+                :max="stockWithPrices?.size"><span>(Total: {{
+                        stockWithPrices?.size
+                }})</span>
+        </div>
+        <div ref="divChart" id="divChart"></div>
     </div>
 </template>
 
 <script setup lang="ts">
 // https://raw.githubusercontent.com/plotly/datasets/master/all_stocks_5yr.csv
 import * as d3 from 'd3';
-import { useFetch } from '@vueuse/core';
+import { useFetch, useElementSize } from '@vueuse/core';
 import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+import { csv, schemeGnBu } from 'd3';
 
 
 const stockNumber = ref(10);
-const { isFinished, error, data, execute, canAbort, abort } = useFetch("https://raw.githubusercontent.com/plotly/datasets/master/all_stocks_5yr.csv", { immediate: false })
-
-const allRows = computed(() => d3.csvParse(data.value as string))
-//const stockWithPrices = computed(() => d3.group(allRows.value, (d)=>d.Name))
-watch(allRows, (newRows, oldRows) => console.log(newRows));
+const fetchStocks = useFetch("https://raw.githubusercontent.com/plotly/datasets/master/all_stocks_5yr.csv", { immediate: false })
 
 onMounted(() => {
-    execute();
+    fetchStocks.execute();
 });
 
 onUnmounted(() => {
-    if (canAbort.value)
-        abort();
+    if (fetchStocks.canAbort.value)
+        fetchStocks.abort();
 })
+
+const allRows = computed(() => fetchStocks.isFinished.value && fetchStocks.data.value ? d3.csvParse(fetchStocks.data.value as string) : null)
+const stockWithPrices = computed(() => allRows.value !== null ? d3.group(allRows.value, (d) => d.Name) : null)
+
+const divChart = ref<HTMLDivElement | null>(null);
+const divChartSize = useElementSize(divChart)
+
+watch([stockNumber, stockWithPrices, divChartSize.width], () => {
+    renderChart();
+})
+
+
+
+function renderChart() {
+    if (!stockWithPrices.value)
+        return;
+    if (!divChart.value)
+        return;
+    const width = Math.floor(d3.min([1000, divChartSize.width.value])!);
+    const height = 600;
+    const margin = { left: 20, top: 20, right: 20, bottom: 20, }
+    const innerWidth = width - margin.left - margin.right
+    const innerHeight = height - margin.top - margin.bottom;
+
+    console.log(`Rendering chart ${width} / ${height} - ${stockNumber.value} stocks`)
+
+    const xScale = d3.scaleLinear().domain([0, 10]).range([0, innerWidth]);
+    const yScale = d3.scaleLinear().domain([0, 10]).range([0, innerHeight]);
+
+    const xAxis = d3.axisLeft(xScale);
+    const yAxis = d3.axisBottom(yScale);
+
+    let svg = d3
+        .select(divChart.value)
+        .selectAll("svg")
+        .data([0])
+        .join(
+            enter => enter.append("svg"),
+            update=> update
+        )
+        .attr("width", width)
+        .attr("height", height)
+        .style("background-color", "red")
+        .call(xAxis as any)
+        .call(yAxis as any)
+}
 
 </script>
     
@@ -48,5 +93,11 @@ onUnmounted(() => {
 .setting {
     display: flex;
     gap: 1em;
+}
+
+#divChart {
+    min-width: 100%;
+    max-width: 100%;
+    min-height: 10;
 }
 </style>
