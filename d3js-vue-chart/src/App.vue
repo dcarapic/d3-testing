9f8326c94251d1906c478e856f9847d04bd2fe07
Dalibor Čarapić <dcarapic@gmail.com>
2022-06-11 20:02:01 +0200

    Playing around

--- d3js-vue-chart/src/App.vue
+++ d3js-vue-chart/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-    <ChartVue />
+    <div class="container">
+        <div class="settings">
+            <label for="stocks">Stocks count</label><input type="number" id="stocks" v-model.lazy="stockNumber" min="10"
+                max="10000"><span>(Max: 10000)</span>
+            <label for="weeks">History</label><input type="number" id="weeks" v-model.lazy="weekNumber" min="1"
+                max="1000"><span>(Max: 1000)</span>
+        </div>
+        <ChartVue :stocks="stocks" />
+        <input type="range" min="0" :max="weekNumber-1" v-model="currentWeek"/>
+        {{currentWeek}} {{typeof(currentWeek)}}
+    </div>
 </template>
 
 <script setup lang="ts">
+import * as d3 from "d3";
+import { computed, ref, shallowRef, watch } from "vue";
+import { GenerateStocks, StockItem } from "./data/stockData";
 import ChartVue from "./components/Chart.vue";
+
+const stockNumber = ref(500);
+const weekNumber = ref(52 * 3);
+const currentWeek = ref(0);
+const allStocks = computed(()=> GenerateStocks(stockNumber.value, weekNumber.value, d3.timeDay()));
+const stocks = computed(() => allStocks.value.filter(d=>d.weekOffset == currentWeek.value));
+
 </script>
 
 <style>
@@ -12,5 +32,19 @@ body {
     margin: 0;
     padding: 0;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.settings 
+{
+    max-width: 1000px;
+    display: grid;
+    gap: 0.2em;
+    grid-template-columns: 100px 80px 100px;
 }
 </style>
--- d3js-vue-chart/src/components/Chart.vue
+++ d3js-vue-chart/src/components/Chart.vue
@@ -1,89 +1,48 @@
 <template>
-    <div class="container">
-        <div v-if="!fetchStocks.isFinished.value">Loading stock data</div>
-        <div v-if="fetchStocks.error.value">An error occurred while loading stock data: {{ fetchStocks.error.value }}
-        </div>
-        <div v-if="fetchStocks.isFinished.value" class="setting">
-            <label for="stocks">Stocks count</label><input type="number" v-model.lazy="stockNumber" min="10"
-                :max="stockWithPrices?.length"><span>(Total: {{
-                        stockWithPrices?.length
-                }})</span>
-        </div>
-        <div id="divChart" ref="divChart">
-            <svg id="svgChart" ref="svgChart" :width="width" :height="height">
-                <g id="xaxis" />
-                <g id="yaxis" />
-                <g id="marks" />
-            </svg>
-        </div>
+    <div ref="divChart" class="chart-container">
+        <svg id="svgChart" ref="svgChart" :width="width" :height="height">
+            <g id="xaxis" />
+            <g id="yaxis" />
+            <g id="marks" />
+        </svg>
     </div>
 </template>
 
 <script setup lang="ts">
-// https://raw.githubusercontent.com/plotly/datasets/master/all_stocks_5yr.csv
 import * as d3 from 'd3';
-import { useFetch, useElementSize } from '@vueuse/core';
-import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+import { useElementSize } from '@vueuse/core';
+import { computed, ref, watch } from 'vue';
+import { StockItem } from '../data/stockData'
 
-type Stock = { name: string, values: Array<{ date: Date, price: number }> };
-
-const stockNumber = ref(10);
-const fetchStocks = useFetch("https://raw.githubusercontent.com/plotly/datasets/master/all_stocks_5yr.csv", { immediate: false })
-
-onMounted(() => {
-    fetchStocks.execute();
-});
-
-onUnmounted(() => {
-    if (fetchStocks.canAbort.value)
-        fetchStocks.abort();
-})
-
-const stockWithPrices = computed(() => {
-    if (!fetchStocks.isFinished.value || fetchStocks.data.value == null || fetchStocks.error.value)
-        return;
-    const allRows = d3.csvParse(fetchStocks.data.value as string)
-    let groups = [...d3.group(allRows, d => d.Name).entries()];
-    //console.log(groups);
-    return groups.map((d) => ({
-        name: d[0] as string,
-        values: d[1].map((r) => ({
-            date: new Date(r["date"] as string),
-            price: parseFloat(r["open"] as string)
-        }))
-    }));
-});
+const props = defineProps<{ stocks: StockItem[] }>();
 
 const divChart = ref<HTMLDivElement | null>(null);
-const svgChart = ref<SVGElement | null>(null);
 const divChartSize = useElementSize(divChart)
 
+const svgChart = ref<SVGElement | null>(null);
+
 const width = computed(() => Math.floor(d3.min([1000, divChartSize.width.value])!));
-const height = computed(() => 600);
+const height = computed(() => Math.floor(width.value * 9 / 16));
 const margin = { left: 25, top: 20, right: 20, bottom: 25 };
 const innerWidth = computed(() => width.value - margin.left - margin.right);
 const innerHeight = computed(() => height.value - margin.top - margin.bottom);
 
-watch([svgChart, stockNumber, stockWithPrices, divChartSize.width], () => {
+watch([svgChart, props, divChartSize.width], () => {
     updateChart();
 })
 
-
-
 function updateChart() {
-    if (!stockWithPrices.value)
-        return;
     if (!svgChart.value)
         return;
+    const stocks = props.stocks || [];
 
-    console.log(`Rendering chart ${width.value} / ${height.value} - ${stockNumber.value} stocks`)
+    console.log(`Rendering chart ${width.value} / ${height.value} - ${stocks.length} stocks`)
 
     const xScale = d3.scaleLinear().domain([0, 10]).range([0, innerWidth.value]);
     const yScale = d3.scaleLinear().domain([0, 10]).range([innerHeight.value, 0]);
+    const rScale = d3.scaleLinear().domain([d3.min(stocks, d => d.size) || 0, d3.max(stocks, d => d.size) || 0]).range([0, 20]);
+    const colorScale = d3.scaleLinear().domain(rScale.domain()).range(["red", "green"] as any);
 
-    const maxPrice = d3.max(stockWithPrices.value, (d) => d.values[d.values.length-1].price) ?? 10;
-    const xValue = d3.scaleLinear().domain([0, maxPrice]).range([0, 10])
-    const yValue = d3.scaleLinear().domain([0, maxPrice]).range([0, 10])
 
     const xAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
         g
@@ -100,16 +59,16 @@ function updateChart() {
         .attr("height", height.value)
     svg.select("g#xaxis").call(xAxis as any)
     svg.select("g#yaxis").call(yAxis as any)
-    const data = stockWithPrices.value.slice(0, stockNumber.value);
 
     const markPane = svg.select("g#marks").attr("transform", `translate(${margin.left}, ${margin.top})`)
     markPane
         .selectAll("circle")
-        .data(data)
+        .data(stocks)
         .join("circle")
-        .attr("cx", (d) => xScale(xValue(d.values[d.values.length-1].price)))
-        .attr("cy", (d) => xScale(xValue(d.values[d.values.length-1].price)))
-        .attr("r", 5)
+        .attr("cx", (d) => xScale(d.alphaX))
+        .attr("cy", (d) => yScale(d.alphaY))
+        .attr("r", (d) => rScale(d.size))
+        .attr("fill", d=>colorScale(d.size))
     //console.log(data);
 }
 
@@ -118,26 +77,9 @@ function updateChart() {
     
 
 <style scoped>
-.container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.setting {
-    display: flex;
-    gap: 1em;
-}
-
-#divChart {
-    min-width: 100%;
-    max-width: 100%;
-    min-height: 10;
+.chart-container {
     display: grid;
     place-items: center;
-}
-
-#svgChart {
-    border: 1px solid red;
+    min-width: 100%;
 }
 </style>
